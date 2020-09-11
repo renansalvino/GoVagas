c79d791fab7d5f73e03d05e9faebe55ca089a808
renansalvino <martinsrenan358@gmail.com>
2020-09-11 20:01:55 -0300

    Fazer testes no Homem Carta

---
+++ Back-End/API/GoVagas/Interfaces/ICandidatoRepository.cs
@@ -0,0 +1,18 @@
+﻿using GoVagas.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoVagas.Interfaces
+{
+    interface ICandidatoRepository
+    {
+       List<Candidato> ListarTodos();
+       public Candidato BuscarPorId(int id);
+       void Cadastrar (Candidato novoCandidato);
+       void Atualizar (int id,Candidato candidatoAtualizado);
+       void Deletar (int id);
+
+    }
+}
---
+++ Back-End/API/GoVagas/Repositories/CandidatoRepository.cs
@@ -0,0 +1,51 @@
+﻿using GoVagas.Contexts;
+using GoVagas.Domains;
+using GoVagas.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoVagas.Repositories
+{
+    public class CandidatoRepository : ICandidatoRepository
+    {
+
+        GoVagasContext ctx = new GoVagasContext();
+
+        public void Atualizar(int id, Candidato candidatoAtualizado)
+        {
+            Candidato candidatoBuscado =  ctx.Candidato.Find(id);
+            ctx.Candidato.Update(candidatoBuscado);
+            ctx.SaveChanges();
+
+        }
+
+        public Candidato BuscarPorId(int id)
+        {
+            return ctx.Candidato.FirstOrDefault(ca => ca.IdCandidato == id);
+        }
+
+        public void Cadastrar(Candidato novoCandidato)
+        {
+            ctx.Candidato.Add(novoCandidato);
+            ctx.SaveChanges();
+        }
+        
+
+        public void Deletar(int id)
+        {
+            Candidato candidatoBuscado = ctx.Candidato.Find(id);
+            ctx.Candidato.Remove(candidatoBuscado);
+            ctx.SaveChanges();
+        }
+
+
+       
+
+        public List<Candidato> ListarTodos()
+        {
+            return ctx.Candidato.ToList();
+        }
+    }
+}
