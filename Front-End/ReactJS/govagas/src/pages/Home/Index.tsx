bbe5a5c437d92c9111386e6f30e2207498f2bc04
Renansalvino <renansalvinomartins454@gmail.com>
2020-10-21 22:49:25 -0300

    [CHECK]Padronizagem dos componentes

--- Front-End/ReactJS/govagas/src/pages/Home/Index.tsx
+++ Front-End/ReactJS/govagas/src/pages/Home/Index.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import './Style.css';
-
+import React from 'react';
+import './style.css';
+import '../../assets/Styles/global.css';
+import Footer from '../../components/footer/index';
+import Header from '../../components/header/index'
+import Button from '../../components/button/index';
+import Input from '../../components/input/index';
 
 function Home() {
     return (
-        <div className='Principal'>
-            <h1>Exemplos de botões com Bootstrap</h1>
-            <div>
-                <button type="button" className="btn btn-info">Botão_de_teste</button>
+        <div className='principal'>
+            <Header />
+            <div className="centro-home">
+                <h1>Exemplos de botões do figma</h1>
+                <div className="Button_Figma">
+                    <div className="davi">
+                        <p>Botão primário</p>
+                        <Button onClick="" name="btn1" value="Salvar" />
+                    </div>
+                    <div className="davi">
+                        <p>Botão secundário</p>
+                        <Button onClick="" value="Cancelar" name="btn2" />
+                    </div>
+                    <div className="davi">
+                        <p>Terceiro botão</p>
+                        <Button onClick="" value="Sair" name="btn3" />
+                    </div>
+                    <div className="Input">
+                        <Input type="password" name="input" label="Input" placeholder="Máximo sua tia" minLength={8} maxLength={100}/>
+                    </div>
+                    <br/>
+                </div>
             </div>
-            <h1>Exemplos de botões do figma</h1>
-            <div className="Button_Figma">
-                <button className="figma1" >Botão_de_teste</button>
-                <button className="figma2">Salvar</button>
-            </div>
+            <Footer />
         </div>
     )
 }
--- Front-End/ReactJS/govagas/src/routers.tsx
+++ Front-End/ReactJS/govagas/src/routers.tsx
@@ -1,13 +1,13 @@
-import React from 'react' 
-import {BrowserRouter, Route } from 'react-router-dom' ;
-import Home from './pages/Home/Index' ;
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/home/index'
 // 	import Login from ‘./pages/Login’;
 
-	function Routers( ) {
-		return(
-			 <BrowserRouter>
-			  <Route path='/' exact component={Home}/>
- 			 </BrowserRouter>
-		);
-	}
+function Routers() {
+	return (
+		<BrowserRouter>
+			<Route path='/' exact component={Home}/>
+		</BrowserRouter>
+	);
+}
 export default Routers;
